f4f50fba3c25e39ede830817652cd7afaf4eee43
CarrotsMedia <carrotsmediaprod@gmail.com>
2026-06-11 13:26:01 +0300

    chore: clean up temporary scripts and benchmark results

--- scratch/check_schema.js
+++
@@ -1,8 +0,0 @@
-const Database = require('c:/Users/PC-3/Downloads/store/node_modules/better-sqlite3');
-const db = new Database('c:/Users/PC-3/Downloads/store/pc_parts.db');
-
-console.log('--- products schema ---');
-console.log(db.prepare("SELECT sql FROM sqlite_master WHERE type='table' AND name='products'").get().sql);
-
-console.log('\n--- product_families schema ---');
-console.log(db.prepare("SELECT sql FROM sqlite_master WHERE type='table' AND name='product_families'").get().sql);
